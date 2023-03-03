739050dc9c174e281df2f86f41349958fd13cb6f
DarwinC2 <darwinc2dallas@gmail.com>
2023-03-03 15:16:14 -0500

    Vehicle Add Form

---
+++ CustomsVehicleAddForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Jamcheck
+{
+    partial class CustomsVehicleAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Make = new System.Windows.Forms.Label();
+            this.VehicleType = new System.Windows.Forms.Label();
+            this.txBoxMake = new System.Windows.Forms.TextBox();
+            this.txBoxVehicleType = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(326, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(471, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vehicle Add Form";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 81);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1207, 581);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(597, 575);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txBoxVehicleType);
+            this.panel2.Controls.Add(this.txBoxMake);
+            this.panel2.Controls.Add(this.VehicleType);
+            this.panel2.Controls.Add(this.Make);
+            this.panel2.Location = new System.Drawing.Point(606, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(597, 575);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Jamcheck.Properties.Resources.Jamcheck_logo_transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(594, 569);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Make
+            // 
+            this.Make.AutoSize = true;
+            this.Make.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Make.Location = new System.Drawing.Point(103, 177);
+            this.Make.Name = "Make";
+            this.Make.Size = new System.Drawing.Size(76, 30);
+            this.Make.TabIndex = 0;
+            this.Make.Text = "Make";
+            // 
+            // VehicleType
+            // 
+            this.VehicleType.AutoSize = true;
+            this.VehicleType.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VehicleType.Location = new System.Drawing.Point(62, 310);
+            this.VehicleType.Name = "VehicleType";
+            this.VehicleType.Size = new System.Drawing.Size(161, 30);
+            this.VehicleType.TabIndex = 1;
+            this.VehicleType.Text = "Vehicle Type";
+            // 
+            // txBoxMake
+            // 
+            this.txBoxMake.Location = new System.Drawing.Point(267, 183);
+            this.txBoxMake.Name = "txBoxMake";
+            this.txBoxMake.Size = new System.Drawing.Size(271, 26);
+            this.txBoxMake.TabIndex = 2;
+            // 
+            // txBoxVehicleType
+            // 
+            this.txBoxVehicleType.Location = new System.Drawing.Point(269, 316);
+            this.txBoxVehicleType.Name = "txBoxVehicleType";
+            this.txBoxVehicleType.Size = new System.Drawing.Size(269, 26);
+            this.txBoxVehicleType.TabIndex = 3;
+            // 
+            // CustomsVehicleAddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 674);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomsVehicleAddForm";
+            this.Text = "Vehicle Add Form";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txBoxVehicleType;
+        private System.Windows.Forms.TextBox txBoxMake;
+        private System.Windows.Forms.Label VehicleType;
+        private System.Windows.Forms.Label Make;
+    }
+}
